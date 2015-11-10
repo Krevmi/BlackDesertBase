77091cdb627788c6cca6038b6dcf02fe6525eb99
KrEvM <87Evgen@gmail.com>
2015-11-10 21:05:29 +0300

    Cooking fragment add

---
+++ app/src/main/java/by/krevm/blackdesertbase/Fragments/CookingFragment.java
@@ -0,0 +1,29 @@
+package by.krevm.blackdesertbase.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import by.krevm.blackdesertbase.Adapters.TabPagerFragmentAdapter;
+import by.krevm.blackdesertbase.R;
+
+
+public class CookingFragment extends Fragment {
+    ViewPager viewPager;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.cooking_fragment,container,false);
+        viewPager = (ViewPager)view.findViewById(R.id.view_pager);
+        TabLayout tabLayout= (TabLayout)view.findViewById(R.id.tab_layout);
+        TabPagerFragmentAdapter adapter = new TabPagerFragmentAdapter(getActivity().getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+        return view;
+    }
+}
